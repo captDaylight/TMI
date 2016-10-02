909dceccca4b3135209e89e1b5e5102ec76360ef
paul <paul.christophe6@gmail.com>
2016-10-01 23:18:00 -0400

    stable-ish

--- bot.js
+++ bot.js
@@ -20,6 +20,23 @@ var db = new JsonDB('data', true, false);
 var data = db.getData('/chat');
 
 const { hears } = controller;
+const patterns = {
+  good: /^(good|great|awesome|alright|tight|sweet)/i,
+  bad: /^(bad|not|terrible|shit|crap|turd|fuck)/i
+};
+
+const introPhrases = [
+  'Hi! I’m Buddy',
+  'Woof woof! Lol',
+  'You can trust me, because I am a dog',
+  `WOOF! I'm a dog`
+];
+
+const simpleQuestions = [
+  'What’s your name?',
+  'Do you like to party?',
+  'Where are you from?',
+];
 
 const saveTMI = (index, tmi) => {
   const data = db.getData('/chat');
@@ -34,31 +51,34 @@ controller.on('direct_message,direct_mention,mention', function(bot, message) {
   const index = Math.floor(Math.random() * data.length);
   const problem = data[index];
 
-  // saveTMI(index, 'hello');
-
   const tellSomethingPrivate = (response, convo) => {
     const ranTMIIndex = Math.floor(Math.random() * problem.tmi.length);
     bot.reply(message, `${problem.intro}, ${problem.tmi[ranTMIIndex]}`);
-    console.log('----setting timeout!!');
-    setTimeout(() => {
-
-      askSomethingPrivate(response, convo);
-      convo.next();
-    }, 100);
-    // askSomethingPrivate(response, convo);
+    bot.reply(message, '...');
+    askSomethingPrivate(response, convo);
+    convo.next();
   }
 
   const askSomethingPrivate = (response, convo) => {
     convo.ask(problem.question, function(response, convo) {
       const tmi = convo.extractResponse('userTMI');
-      // TODO make sure we're getting the tmi data from the question
-      saveTMI(index, tmi);
+      if (tmi.length > 20) {
+        saveTMI(index, tmi);
+      }
+
+      bot.reply(message, `WOW that's super heavy.`);
+      bot.reply(message, `OK well, great talking to you`);
+      // bot.reply(message, {'image_url': 'http://r.ddmcdn.com/s_f/o_1/cx_633/cy_0/cw_1725/ch_1725/w_720/APL/uploads/2014/11/too-cute-doggone-it-video-playlist.jpg'});
+      bot.reply(message, {attachments: [{
+        "text": "WOOF WOOF",
+        "image_url": "https://media.giphy.com/media/10Fqkgb4tQVtOo/giphy.gif",
+      }]});
       convo.next();
     }, {key: 'userTMI'});
   };
 
-  bot.startConversation(message,function(err,convo) {
-    convo.ask('Can I tell you something',[
+  const askIfCanAsk = (response, convo) => {
+    convo.ask('Can I tell you something', [
       {
         pattern: bot.utterances.yes,
         callback: function(response,convo) {
@@ -84,5 +104,47 @@ controller.on('direct_message,direct_mention,mention', function(bot, message) {
         }
       }
     ]);
+  };
+
+  const askSimpleQuestion = (response, convo, wasPositive = true) => {
+    convo.ask(`${wasPositive ? 'WOOF! Wow so great to hear' : 'OH WOOF WOOF BUMMER'}. ${simpleQuestions[Math.floor(Math.random() * simpleQuestions.length)]}`, [
+      {
+        default: true,
+        callback: function(response,convo) {
+          // just repeat the question
+          askIfCanAsk(response, convo);
+          convo.next();
+        }
+      }
+    ])
+  };
+
+  bot.startConversation(message,function(err,convo) {
+    convo.ask(`${introPhrases[Math.floor(Math.random() * introPhrases.length)]}. how are you?`, [
+      {
+        pattern: patterns.good,
+        callback: function(response,convo) {
+          // do something else...
+          askSimpleQuestion(response, convo, true);
+          convo.next();
+        }
+      },
+      {
+        pattern: patterns.bad,
+        callback: function(response,convo) {
+          // do something else...
+          askSimpleQuestion(response, convo, false);
+          convo.next();
+        }
+      },
+      {
+        default: true,
+        callback: function(response,convo) {
+          // just repeat the question
+          convo.repeat();
+          convo.next();
+        }
+      }
+    ])
   });
 });
